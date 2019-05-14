b65f8158f3f767ead7650f1b3717fa962c498b48
jedesai <jeet.d.desai@gmail.com>
2019-05-13 22:23:14 -0400

    Completed alexa skill

---
+++ AlexaQuotes/test-event-author-quote-correct.js
@@ -0,0 +1,47 @@
+{
+    "version": "1.0",
+    "session": {
+      "new": false,
+      "sessionId": "*****************",
+      "application": {
+        "applicationId": "**************"
+      },
+      "attributes": {},
+      "user": {
+        "userId": "amzn1.ask.account.testUser"
+      }
+    },
+    "context": {
+      "AudioPlayer": {
+        "playerActivity": "IDLE"
+      },
+      "System": {
+        "application": {
+          "applicationId": "*****************"
+        },
+        "user": {
+          "userId": "amzn1.ask.account.testUser"
+        },
+        "device": {
+          "supportedInterfaces": {
+            "AudioPlayer": {}
+          }
+        }
+      }
+    },
+    "request": {
+      "type": "IntentRequest",
+      "requestId": "amzn1.echo-api.request.1234",
+      "timestamp": "2016-10-27T21:06:28Z",
+      "locale": "en-US",
+      "intent": {
+        "name": "AuthorQuote",
+        "slots": {
+            "author": {
+                "name": "author",
+                "value": "Einstein"
+            }
+        }
+      }
+    }
+  }
